e3a929e054af2001ace416d6c66e667e9621545b
Hossein <hossein.ahmed78@gmail.com>
2019-06-25 20:01:12 +0200

    added user modification page

---
+++ src/app/pages/backoff-user/account-modifications/account-modifications.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-modifications',
+  templateUrl: './account-modifications.page.html',
+  styleUrls: ['./account-modifications.page.scss'],
+})
+export class AccountModificationsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
