bca59ff72531efb0b1d8901e049a87f81790f1f5
unknown <hossein.ahmed78@gmail.com>
2019-07-05 14:41:18 +0200

    Added solder page

---
+++ src/app/pages/backoff-cantiniere/solder/solder.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { SolderPage } from './solder.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SolderPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [SolderPage]
+})
+export class SolderPageModule {}
---
+++ src/app/pages/backoff-cantiniere/solder/solder.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solder',
+  templateUrl: './solder.page.html',
+  styleUrls: ['./solder.page.scss'],
+})
+export class SolderPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
