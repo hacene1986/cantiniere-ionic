2eba66a26f76b96a57b36ef3995a15043d5a2fc3
unknown <hossein.ahmed78@gmail.com>
2019-07-05 14:41:02 +0200

    Added recap cantiniere page

---
+++ src/app/pages/backoff-cantiniere/recap-cantiniere/recap-cantiniere.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recap-cantiniere',
+  templateUrl: './recap-cantiniere.page.html',
+  styleUrls: ['./recap-cantiniere.page.scss'],
+})
+export class RecapCantinierePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
