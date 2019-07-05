afd4b7510eb52b0e013b6247063485fa7feeab6b
unknown <hossein.ahmed78@gmail.com>
2019-07-05 14:39:07 +0200

    Added crediter page

---
+++ src/app/pages/backoff-cantiniere/crediter/crediter.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crediter',
+  templateUrl: './crediter.page.html',
+  styleUrls: ['./crediter.page.scss'],
+})
+export class CrediterPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
