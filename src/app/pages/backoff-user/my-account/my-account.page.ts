75df0869d6df5e5d2c42296ec59b57a1a3e25f56
Hossein <hossein.ahmed78@gmail.com>
2019-07-21 13:50:05 +0200

    added logic to pages

--- src/app/pages/backoff-user/my-account/my-account.page.ts
+++ src/app/pages/backoff-user/my-account/my-account.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import jwt_decod from 'jwt-decode'
 
 @Component({
   selector: 'app-my-account',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-account.page.scss'],
 })
 export class MyAccountPage implements OnInit {
-
-  constructor() { }
+  id: string;
+  user: Object;
+  constructor( private userService: UserService,) {}
 
   ngOnInit() {
+    this.getUser()
+  }
+
+  getUser(){
+    let tok = localStorage.getItem('token')
+   let decod = jwt_decod(tok)
+    console.log(decod.user)
+    this.user = decod.user
   }
 
 }
