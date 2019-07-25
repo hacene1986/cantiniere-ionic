5f02ec63bc32e38674f96aa6e7ecf1d8a34a5610
Hossein <hossein.ahmed78@gmail.com>
2019-07-25 20:26:40 +0200

    added users orders for menus

--- src/app/pages/backoff-cantiniere/recap-cantiniere/recap-cantiniere.page.ts
+++ src/app/pages/backoff-cantiniere/recap-cantiniere/recap-cantiniere.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Order } from "src/app/models/order";
+import { OrderService } from "src/app/services/order.service";
 
 @Component({
-  selector: 'app-recap-cantiniere',
-  templateUrl: './recap-cantiniere.page.html',
-  styleUrls: ['./recap-cantiniere.page.scss'],
+  selector: "app-recap-cantiniere",
+  templateUrl: "./recap-cantiniere.page.html",
+  styleUrls: ["./recap-cantiniere.page.scss"]
 })
 export class RecapCantinierePage implements OnInit {
-
-  constructor() { }
+  orders: Array<Order>;
+  oderMenu: any;
+  constructor(private OrderService: OrderService) {}
 
   ngOnInit() {
+    this.recapOrder();
   }
 
+  recapOrder() {
+    this.OrderService.getAllOrder().subscribe(res => {
+      console.log(res);
+      this.orders = res;
+    });
+  }
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,66 +1,115 @@
-import { Constraint } from '../models/constraint';
-import { User } from '../models/user';
-import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
-import { Order } from '../models/order';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Constraint } from "../models/constraint";
+import { User } from "../models/user";
+import { environment } from "../../environments/environment";
+import { Observable } from "rxjs";
+import { Order } from "../models/order";
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class OrderService {
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   // Méthode pour ajouter une commande
   addOrder(order: Order): Observable<Order> {
-    return this.httpClient.put<Order>(environment.urlServeurBackEnd + 'order/add/', order);
+    return this.httpClient.put<Order>(
+      environment.urlServeurBackEnd + "order/add/",
+      order
+    );
   }
 
   // Méthode pour annuler une commande
   cancelOrder(order: Order): Observable<Order> {
-    return this.httpClient.patch<Order>(environment.urlServeurBackEnd + 'order/cancel/' + order.id, order);
+    return this.httpClient.patch<Order>(
+      environment.urlServeurBackEnd + "order/cancel/" + order.id,
+      order
+    );
   }
 
   // Méthode pour récuperer une commande
   getOrder(order: Order): Observable<Order> {
-    return this.httpClient.get<Order>(environment.urlServeurBackEnd + 'order/find/' + order.id);
+    return this.httpClient.get<Order>(
+      environment.urlServeurBackEnd + "order/find/" + order.id
+    );
   }
 
   // Méthode pour récupérer toutes les commandes
   getAllOrder(): Observable<Order[]> {
-    return this.httpClient.get<Order[]>(environment.urlServeurBackEnd + 'order/findall/');
+    let reqHeader = new HttpHeaders({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token")
+    });
+    return this.httpClient.get<Order[]>(
+      environment.urlServeurBackEnd + "order/findall/",
+      { headers: reqHeader }
+    );
   }
 
   // Méthode pour récupérer toute les commandes de tous les utilisateurs en fonction des parametres
-  getAllOrderBetweenDateInStatus(beginDate: string, endDate: string, status: number): Observable<Order[]> {
-    return this.httpClient.get<Order[]>(environment.urlServeurBackEnd + 'order/findallbetweendateinstatus/');
+  getAllOrderBetweenDateInStatus(
+    beginDate: string,
+    endDate: string,
+    status: number
+  ): Observable<Order[]> {
+    return this.httpClient.get<Order[]>(
+      environment.urlServeurBackEnd + "order/findallbetweendateinstatus/"
+    );
   }
 
   // Méthode pour récupérer toutes les commandes d'un utilisateur
-  getAllOrderForUser(beginDate: string, endDate: string, status: number, user: User): Observable<Order[]> {
-    return this.httpClient.get<Order[]>(environment.urlServeurBackEnd + 'order/findallforuser/' + user.id);
+  getAllOrderForUser(
+    beginDate: string,
+    endDate: string,
+    status: number,
+    user: User
+  ): Observable<Order[]> {
+    return this.httpClient.get<Order[]>(
+      environment.urlServeurBackEnd + "order/findallforuser/" + user.id
+    );
   }
 
   // Méthode pour récupérer toutes les commandes d'aujourd'hui, d'un utilisateur
-  getAllOrderForTodayByUser(beginDate: string, endDate: string, status: number, user: User): Observable<Order[]> {
-    return this.httpClient.get<Order[]>(environment.urlServeurBackEnd + 'order/findallforusertoday/' + user.id);
+  getAllOrderForTodayByUser(
+    beginDate: string,
+    endDate: string,
+    status: number,
+    user: User
+  ): Observable<Order[]> {
+    return this.httpClient.get<Order[]>(
+      environment.urlServeurBackEnd + "order/findallforusertoday/" + user.id
+    );
   }
 
   // Méthode pour update une commande
   updateOrder(orderId: number, order: Order): Observable<Order> {
-    return this.httpClient.patch<Order>(environment.urlServeurBackEnd + 'order/update/' + orderId, order);
+    return this.httpClient.patch<Order>(
+      environment.urlServeurBackEnd + "order/update/" + orderId,
+      order
+    );
   }
 
   // Méthode pour calculer le prix de la commande
   computeOrderPrice(contrainteId: number, orderId: number): Observable<Order> {
-    return this.httpClient.get<Order>(environment.urlServeurBackEnd + 'order/computeprice/' + orderId + '/' + contrainteId);
+    return this.httpClient.get<Order>(
+      environment.urlServeurBackEnd +
+        "order/computeprice/" +
+        orderId +
+        "/" +
+        contrainteId
+    );
   }
 
   // Méthode pour payer et délivrer la commande
   payOrder(constraint: Constraint, order: Order): Observable<Order> {
-    return this.httpClient.patch<Order>(environment.urlServeurBackEnd + 'order/deliverandpay/' + order.id + '/' + constraint.id, order);
+    return this.httpClient.patch<Order>(
+      environment.urlServeurBackEnd +
+        "order/deliverandpay/" +
+        order.id +
+        "/" +
+        constraint.id,
+      order
+    );
   }
-
 }
