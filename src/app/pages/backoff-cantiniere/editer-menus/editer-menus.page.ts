2f103bb7edee7f11e903e13bfd310577a60c6a34
Hossein <hossein.ahmed78@gmail.com>
2019-07-27 20:25:35 +0200

    edit menus from back office

--- src/app/pages/backoff-cantiniere/editer-menus/editer-menus.page.ts
+++ src/app/pages/backoff-cantiniere/editer-menus/editer-menus.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MenuService } from "../../../services/menu.service";
+import { NgForm } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-editer-menus',
-  templateUrl: './editer-menus.page.html',
-  styleUrls: ['./editer-menus.page.scss'],
+  selector: "app-editer-menus",
+  templateUrl: "./editer-menus.page.html",
+  styleUrls: ["./editer-menus.page.scss"]
 })
 export class EditerMenusPage implements OnInit {
+  id: string;
+  menu: Object;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private menuService: MenuService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.route.params.subscribe(params => (this.id = params.id));
   }
 
+  ngOnInit() {
+    this.getMenu(this.id);
+  }
+
+  getMenu(id) {
+    console.log(id);
+    this.menuService.getMenu(id).subscribe(res => {
+      console.log(res);
+      this.menu = res;
+    });
+  }
+
+  editMenu(form: NgForm) {
+    console.log(form.form.value);
+    this.menuService.updateMenu(this.id, form.form.value).subscribe(menu => {
+      console.log("ok");
+      form.reset();
+      window.location.reload();
+    });
+  }
 }
