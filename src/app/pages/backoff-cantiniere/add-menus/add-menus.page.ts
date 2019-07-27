e595794d142bc457b350d1dbb18e8682c60b2ab8
Hossein <hossein.ahmed78@gmail.com>
2019-07-27 20:23:54 +0200

    add menus from back office

--- src/app/pages/backoff-cantiniere/add-menus/add-menus.page.ts
+++ src/app/pages/backoff-cantiniere/add-menus/add-menus.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Menu } from "../../../models/menu";
+import { MenuService } from "src/app/services/menu.service";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-add-menus',
-  templateUrl: './add-menus.page.html',
-  styleUrls: ['./add-menus.page.scss'],
+  selector: "app-add-menus",
+  templateUrl: "./add-menus.page.html",
+  styleUrls: ["./add-menus.page.scss"]
 })
 export class AddMenusPage implements OnInit {
+  viewMode = "tabToday";
+  weekNumber = 49;
+  listMenuThisWeek: Array<Menu>;
+  listMenuToday: Array<Menu>;
 
-  constructor() { }
+  constructor(private menuService: MenuService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  addMenu(form: NgForm) {
+    const menu: Menu = {
+      label: form.value.label,
+      description: form.value.description,
+      priceDF: form.value.priceDF,
+      image: form.value.image,
+      meals: form.value.meals,
+      availableForWeeks: form.value.availableForWeeks
+    };
+    console.log(menu);
+    this.menuService.addMenu(menu).subscribe(menu => {
+      console.log(menu);
+      form.reset();
+      window.location.reload();
+    });
   }
-
 }
