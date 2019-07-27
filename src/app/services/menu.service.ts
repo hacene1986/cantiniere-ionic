f050d6cc6b2b1143d50c6306f3b4e89d2c0855fe
Hossein <hossein.ahmed78@gmail.com>
2019-07-27 20:28:00 +0200

    updated menu and meal services

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,52 +1,86 @@
-import { environment } from '../../environments/environment';
-import { Menu } from '../models/menu';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { environment } from "../../environments/environment";
+import { Menu } from "../models/menu";
+import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class MenuService {
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   // Méthode pour récuperer tous les menus de la semaine
   getAllMenuForWeek(weekNumber: number): Observable<Menu[]> {
-    return this.httpClient.get<Menu[]>(environment.urlServeurBackEnd + 'menu/findallavailableforweek/' + weekNumber);
+    return this.httpClient.get<Menu[]>(
+      environment.urlServeurBackEnd +
+        "menu/findallavailableforweek/" +
+        weekNumber
+    );
   }
   // Méthode pour récuperer tous les menus du jour
   getAllMenuForToday(): Observable<Menu[]> {
-    return this.httpClient.get<Menu[]>(environment.urlServeurBackEnd + 'menu/findallavailablefortoday');
+    return this.httpClient.get<Menu[]>(
+      environment.urlServeurBackEnd + "menu/findallavailablefortoday"
+    );
   }
 
   // Méthode pour récuperer un menu
-  getMenu(menu: Menu): Observable<Menu> {
-    return this.httpClient.get<Menu>(environment.urlServeurBackEnd + 'menu/find/' + menu);
+  getMenu(id: string): Observable<Menu> {
+    return this.httpClient.get<Menu>(
+      environment.urlServeurBackEnd + "menu/find/" + id
+    );
   }
 
   getMenuById(id: number): Observable<Menu> {
-    return this.httpClient.get<Menu>(environment.urlServeurBackEnd + 'menu/find/' + id);
+    return this.httpClient.get<Menu>(
+      environment.urlServeurBackEnd + "menu/find/" + id
+    );
   }
 
   // PROCHAINES METHODES SEULEMENT POUR LA CANTINIERE
   // Méthode pour récuperer tous les menus
   getAllMenu(): Observable<Menu[]> {
-    return this.httpClient.get<Menu[]>(environment.urlServeurBackEnd + 'menu/findall');
+    return this.httpClient.get<Menu[]>(
+      environment.urlServeurBackEnd + "menu/findall"
+    );
   }
 
   // Méthode pour ajouter un menu
   addMenu(menu: Menu): Observable<Menu> {
-    return this.httpClient.post<Menu>(environment.urlServeurBackEnd + 'menu/add', menu);
+    let reqHeader = new HttpHeaders({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token")
+    });
+    return this.httpClient.put<Menu>(
+      environment.urlServeurBackEnd + "menu/add",
+      menu,
+      { headers: reqHeader }
+    );
   }
 
   // Méthode pour supprimer un menu
-  deleteMenu(menu: Menu): Observable<Menu> {
-    return this.httpClient.delete<Menu>(environment.urlServeurBackEnd + 'menu/delete/' + menu);
+  deleteMenu(id: string): Observable<Menu> {
+    let reqHeader = new HttpHeaders({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token")
+    });
+    return this.httpClient.delete<Menu>(
+      environment.urlServeurBackEnd + "menu/delete/" + id,
+      { headers: reqHeader }
+    );
   }
 
   // Méthode pour update un menu
-  updateMenu(menuId: number, menu: Menu): Observable<Menu> {
-    return this.httpClient.patch<Menu>(environment.urlServeurBackEnd + 'menu/update/' + menuId, menu);
+  updateMenu(id: string, menu: Menu): Observable<Menu> {
+    let reqHeader = new HttpHeaders({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token")
+    });
+    return this.httpClient.patch<Menu>(
+      environment.urlServeurBackEnd + "menu/update/" + id,
+      menu,
+      { headers: reqHeader }
+    );
   }
 }
--- src/app/services/plat.service.ts
+++ src/app/services/plat.service.ts
@@ -1,49 +1,81 @@
-import { Observable } from 'rxjs';
-import { Meal } from '../models/meal';
-import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { Meal } from "../models/meal";
+import { environment } from "../../environments/environment";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PlatService {
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   // Méthode pour récupérer tous les plats de la semaine
   getAllMealsForWeek(weekNumber: number): Observable<Meal[]> {
-    return this.httpClient.get<Meal[]>(environment.urlServeurBackEnd + 'meal/findallavailableforweek/' + weekNumber);
+    return this.httpClient.get<Meal[]>(
+      environment.urlServeurBackEnd +
+        "meal/findallavailableforweek/" +
+        weekNumber
+    );
   }
 
   // Méthode pour récupérer tous les plats du jour
   getAllMealsForToday(): Observable<Meal[]> {
-    return this.httpClient.get<Meal[]>(environment.urlServeurBackEnd + 'meal/findallavailablefortoday');
+    return this.httpClient.get<Meal[]>(
+      environment.urlServeurBackEnd + "meal/findallavailablefortoday"
+    );
   }
 
   // Méthode pour récupérer un plat
-  getMeal(meal: Meal): Observable<Meal> {
-    return this.httpClient.get<Meal>(environment.urlServeurBackEnd + 'meal/find/' + meal);
+  getMeal(id: string): Observable<Meal> {
+    return this.httpClient.get<Meal>(
+      environment.urlServeurBackEnd + "meal/find/" + id
+    );
   }
 
   // PROCHAINES METHODES SEULEMENT POUR LA CANTINIERE
   // Méthode pour récupérer tous les plats
   getAllMeal(): Observable<Meal[]> {
-    return this.httpClient.get<Meal[]>(environment.urlServeurBackEnd + 'meal/findall');
+    return this.httpClient.get<Meal[]>(
+      environment.urlServeurBackEnd + "meal/findall"
+    );
   }
 
   // Méthode pour ajouter un plat
   addMeal(meal: Meal): Observable<Meal> {
-    return this.httpClient.post<Meal>(environment.urlServeurBackEnd + 'meal/add', meal);
+    let reqHeader = new HttpHeaders({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token")
+    });
+    return this.httpClient.put<Meal>(
+      environment.urlServeurBackEnd + "meal/add",
+      meal,
+      { headers: reqHeader }
+    );
   }
 
   // Méthode pour supprimer un plat
   deleteMeal(meal: Meal): Observable<Meal> {
-    return this.httpClient.delete<Meal>(environment.urlServeurBackEnd + 'meal/delete/' + meal);
+    let reqHeader = new HttpHeaders({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token")
+    });
+    return this.httpClient.delete<Meal>(
+      environment.urlServeurBackEnd + "meal/delete/" + meal,
+      { headers: reqHeader }
+    );
   }
 
   // Méthode pour update un plat
-  updateMeal(mealId: number, meal: Meal): Observable<Meal> {
-    return this.httpClient.patch<Meal>(environment.urlServeurBackEnd + 'meal/update/' + mealId, meal);
+  updateMeal(id: string, meal: Meal): Observable<Meal> {
+    let reqHeader = new HttpHeaders({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token")
+    });
+    return this.httpClient.patch<Meal>(
+      environment.urlServeurBackEnd + "meal/update/" + id,
+      meal,
+      { headers: reqHeader }
+    );
   }
 }
